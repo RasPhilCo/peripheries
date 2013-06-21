4babb0808a2a7b96633e4f42b712146166c309dd
RasPhilCo <ras@philco>
2013-06-20 23:50:02 -0400

    bootstrap rails. check. closes #6

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Peripheries::Application.config.secret_key_base = '6bdd6c48b36e80c75f1c253f6f10eaa15b17844920addca36c4786666129009e5404f0c0494e5aa97d3ea240c7b30f6bba751cac598e1c0edf3d44efff9c21c4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Peripheries::Application.config.session_store :cookie_store, key: '_peripheries_session'
